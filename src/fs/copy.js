b29d1088053cf931b4b7ffc247e0822631fbe15b
Alex <alex+github@gralak.org>
2025-10-26 00:50:43 +0200

    feat: implement copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+
+const filePath = 'fs/files';
+const fileCopyPath = filePath + '_copy';
+
 const copy = async () => {
-  // Write your code here
+  try {
+    await fs.access(fileCopyPath);
+    throw new Error('FS operation failed'); //dest dir exists if we reach this line
+  } catch (err) {
+    if (err.code !== 'ENOENT') throw err; //consume dest dir does not exists, rethrow others
+  }
+  return fs
+    .cp(filePath, fileCopyPath, {recursive: true, force: false, errorOnExist: true})
+    .catch((err) => {
+      if (err.code === 'ENOENT') throw new Error('FS operation failed'); //source dir does not exists
+    });
 };
 
 await copy();

15d9c158820c55716b54f6a79a8f0e2c5222fb05
Alex <alex+github@gralak.org>
2025-10-26 01:07:42 +0200

    feat: simplified copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -5,16 +5,14 @@ const fileCopyPath = filePath + '_copy';
 
 const copy = async () => {
   try {
-    await fs.access(fileCopyPath);
-    throw new Error('FS operation failed'); //dest dir exists if we reach this line
+    await fs.mkdir(fileCopyPath, {recursive: false});
+    await fs.cp(filePath, fileCopyPath, {recursive: true, force: false, errorOnExist: true})
   } catch (err) {
-    if (err.code !== 'ENOENT') throw err; //consume dest dir does not exists, rethrow others
+    if (err.code === 'EEXIST') throw new Error('FS operation failed'); //destination dir exists
+    if (err.code === 'ENOENT') {
+      await fs.rmdir(fileCopyPath); //it was created while checking for existence
+      throw new Error('FS operation failed'); //source dir does not exists
+    }
   }
-  return fs
-    .cp(filePath, fileCopyPath, {recursive: true, force: false, errorOnExist: true})
-    .catch((err) => {
-      if (err.code === 'ENOENT') throw new Error('FS operation failed'); //source dir does not exists
-    });
 };
-
 await copy();

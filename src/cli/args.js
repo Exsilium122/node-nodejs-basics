6ecf081aa06cbb7f56da0d2079774998330a6dd5
Alex <alex+github@gralak.org>
2025-10-26 17:15:21 +0100

    feat: implement args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-  // Write your code here
+  const args = process.argv.slice(2);
+  const res = [];
+  for (let i = 0; i < args.length; i++) {
+    if (args[i].startsWith('--')) { //assume that those arguments have right format
+      const prop = args[i].slice(2);
+      const val = args[i+1];
+      res.push(`${prop} is ${val}`);
+    }
+    //skip all other parameters or values like
+    //-a aa -b -d ddd
+  }
+  const text = res.join(', ');
+  console.log(text);
 };
+
 
 parseArgs();

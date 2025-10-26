401d7960296560de71fb77d6eda0a247ce4b305c
Alex <alex+github@gralak.org>
2025-10-26 19:14:01 +0100

    feat: implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
-const spawnChildProcess = async (args) => {
-  // Write your code here
+import { spawn } from 'node:child_process';
+
+
+const childPath = './cp/files/script.js';
+
+const spawnChildProcess = async (args = []) => {
+  const child = spawn('node', [childPath, ...args]);
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality

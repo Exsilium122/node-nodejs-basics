b5c4e452f9d8e382ae2692070ca4503989bd657d
Alex <alex+github@gralak.org>
2025-10-26 16:52:30 +0100

    feat: implement list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,13 @@
+import fs from 'fs/promises';
+import { exists } from './utils.js';
+
+
+const filePath = 'fs/files';
+
 const list = async () => {
-  // Write your code here
+  await exists(filePath);
+  const files = await fs.readdir(filePath);
+  console.log(files);
 };
 
 await list();
